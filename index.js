e079f7790b640428ca3000ac29c8bcc255819b4a
bealearnscode <bealearnscode@gmail.com>
2017-10-13 01:04:26 -0400

    add event listener to logo that will toggle inner text between facebook and my name

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let logo = document.querySelector("#logo-to-name-toggle");
-console.log(logo);
+
+logo.addEventListener("click", () => {
+    if (logo.innerText === "Facebook") {
+        logo.innerText = "Beatrix";
+    } else if (logo.innerText === "Beatrix") {
+        logo.innerText = "Facebook";
+    }
+})
